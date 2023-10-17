81eeaabdf50d85ac1ea95a80b59a7933c89c7c7c
Loknathmaity <sohammaity123@gmail.com>
2023-10-17 06:46:54 +0000

    Hi, Commit,all

---
+++ stringbuild.java
@@ -0,0 +1,18 @@
+package Day2;
+
+public class stringbuild {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuilder stringBuilder=new StringBuilder("Learning start");
+		stringBuilder.append(" on day 2");
+//		stringBuilder.insert(8, " Java ");
+//		stringBuilder.replace(4, 9, "Java ");
+//		stringBuilder.reverse();
+		
+//		System.out.println(stringBuilder);
+		System.out.println(stringBuilder.capacity());
+
+	}
+
+}
